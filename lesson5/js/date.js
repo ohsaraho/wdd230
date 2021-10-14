ccf54ff8df3ca8c921d40b23d0738765c152e833
ohsaraho <osg21001@byui.edu>
2021-10-14 00:18:44 -0600

    testing

--- lesson5/js/date.js
+++ lesson5/js/date.js
@@ -8,3 +8,21 @@ const options = {
 };
 
 document.getElementById("datefooter").textContent = date.toLocaleDateString("en-UK", options);
+
+
+
+let dayOfWeek;
+
+dayOfWeek = date.getDay();
+
+const bannerMessage = document.getElementById("bannerDayOfWeek");
+
+
+if (dayOfWeek === 5) {
+    bannerMessage.style.display = 'block';
+    
+} 
+else {
+    bannerMessage.style.display = 'none';
+}
+
