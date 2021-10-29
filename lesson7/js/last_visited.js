3fbb93bfde87b6e177290f5142a8adeaade037d4
ohsaraho <osg21001@byui.edu>
2021-10-29 13:57:12 -0600

    last visited redone

--- lesson7/js/last_visited.js
+++ lesson7/js/last_visited.js
@@ -1,23 +1,40 @@
 let now = Date.now();
 
 let lastVisited = window.localStorage.getItem('lastVisit');
+if (lastVisited != 'undefined') {
+    const oneDay = 24 * 60 * 60 * 1000;
 
-const oneDay = 24 * 60 * 60 * 1000;
+    const numberOfDays = Math.floor((now - lastVisited) / oneDay);
 
-const numberOfDays = Math.floor((now - lastVisited) / oneDay);
-
-window.localStorage.setItem('lastVisit', now);
-
-if (numberOfDays === 0 || numberOfDays >= 2) {
-    document.querySelector('.last_visit_day').textContent = `Last visited: ${numberOfDays} days ago`;
+    if (numberOfDays === 0 || numberOfDays >= 2) {
+        document.querySelector('.last_visit_day').textContent = `Last visited: ${numberOfDays} days ago`;
+    }
+    else if (numberOfDays === 1) {
+        document.querySelector('.last_visit_day').textContent = `Last visit: ${numberOfDays} day ago`;
+    }
 }
-else if (numberOfDays === 1) {
-    document.querySelector('.last_visit_day').textContent = `Last visit: ${numberOfDays} day ago`;
-}
-
 else {
     document.querySelector('.last_visit_day').textContent = `Last visited: never`;
 }
+
+window.localStorage.setItem('lastVisit', now);
+
+// const oneDay = 24 * 60 * 60 * 1000;
+
+// const numberOfDays = Math.floor((now - lastVisited) / oneDay);
+
+// window.localStorage.setItem('lastVisit', now);
+
+// if (numberOfDays === 0 || numberOfDays >= 2) {
+//     document.querySelector('.last_visit_day').textContent = `Last visited: ${numberOfDays} days ago`;
+// }
+// else if (numberOfDays === 1) {
+//     document.querySelector('.last_visit_day').textContent = `Last visit: ${numberOfDays} day ago`;
+// }
+
+// else {
+//     document.querySelector('.last_visit_day').textContent = `Last visited: never`;
+// }
 
 
 
