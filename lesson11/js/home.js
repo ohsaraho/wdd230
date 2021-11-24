9c513eee11c9c8dd02603fc2fe75f0e3f4ea6721
ohsaraho <osg21001@byui.edu>
2021-11-24 11:38:38 -0700

    started the other two towns

---
+++ lesson11/js/home.js
@@ -0,0 +1,47 @@
+const requestURL = 'https://byui-cit230.github.io/weather/data/towndata.json';
+
+
+
+fetch(requestURL)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (jsonObject) {
+    const towns = jsonObject['towns'];
+    // console.table(jsonObject);  // temporary checking for valid response and data parsing
+    towns.forEach(town => { 
+        if (town.name == 'Fish Haven' || town.name == 'Preston' || town.name == 'Soda Springs'){
+          let card = document.createElement('section');
+          let div = document.createElement('div');
+          let h2 = document.createElement('h2');
+          let h3 = document.createElement('h3');
+          let p1 = document.createElement('p');
+          let p2 = document.createElement('p');
+          let p3 = document.createElement('p');
+          let image = document.createElement('img');
+          
+          div.setAttribute('class', 'town_data');
+          image.setAttribute('src', `images/home/${town.photo}`);
+          image.setAttribute('alt', `Picture of ${town.name}`);
+          h2.textContent = town.name;
+          h3.textContent = town.motto;
+          p1.textContent = `Year Founded: ${town.yearFounded}`;
+          p2.textContent = `Population: ${town.currentPopulation}`;
+          p3.textContent = `Annual Rain: ${town.averageRainfall}`;
+
+          card.appendChild(image);
+          card.appendChild(div);
+          div.appendChild(h2);
+          div.appendChild(h3);
+          div.appendChild(p1);
+          div.appendChild(p2);
+          div.appendChild(p3);
+          
+
+
+          document.querySelector('div.cards').appendChild(card);
+        }
+
+    
+  });
+});
---
+++ lesson11/js/soda_springs_api.js
@@ -0,0 +1,44 @@
+const apiURL = "https://api.openweathermap.org/data/2.5/weather?id=5607916&units=imperial&appid=5a69da6bedb5c53762ac5a1761526a1b";
+fetch(apiURL)
+  .then((response) => response.json())
+  .then((jsObject) => {
+    let t = jsObject.main.temp;
+    let s = jsObject.wind.speed;
+    let windchill = 35.74 + (0.6215 * t) - (35.75 *(s ** 0.16)) + (0.4275 * t * (s ** 0.16));
+
+    console.log(jsObject);
+    document.getElementById('current_weather').textContent = jsObject.weather[0].description;
+    document.getElementById('current_temp').textContent = `${jsObject.main.temp} °F`;
+    document.getElementById('humidity').textContent = `${jsObject.main.humidity} %`;
+    document.getElementById('wind_speed').textContent = `${jsObject.wind.speed} mph`;
+    if (t <= 50 && s > 3) {
+        document.querySelector('#wind_chill').textContent = `${Math.round(windchill)} °F`;
+    }
+    
+    else {
+        windchill = 'N/A';
+        document.querySelector('#wind_chill').textContent = 'N/A';
+    }
+    
+  });
+
+const forecastURL = "https://api.openweathermap.org/data/2.5/forecast?id=5607916&units=imperial&appid=5a69da6bedb5c53762ac5a1761526a1b";
+fetch(forecastURL)
+  .then((response) => response.json())
+  .then((jsObject) => {
+  const forecastfive = jsObject.list.filter(item => item.dt_txt.includes('18:00:00'));
+  const weekdays = ['Sun', 'Mon', 'Tues', 'Wed', 'Thurs', 'Fri', 'Sat'];
+  
+  console.log(jsObject);
+  forecastfive.forEach((forecast, index) => {
+    const forcast_day = forecast.dt_txt;
+    let day = new Date(forcast_day).getDay()
+    const imagesrc = `https://openweathermap.org/img/wn/${forecast.weather[0].icon}@2x.png`;
+    index += 1;
+
+    document.querySelector(`.col-head${index}`).textContent = weekdays[day];
+    document.querySelector(`.weather_data${index}`).textContent = `${forecast.main.temp.toFixed(1)}°F`; 
+    document.querySelector(`.weather_icon${index}`).setAttribute('src', imagesrc);
+    document.querySelector(`.weather_icon${index}`).setAttribute('alt', forecast.weather[0].description); 
+    });
+});
