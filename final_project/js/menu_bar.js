ae70ecb464c9b748228002881c803a24f2b56a8a
ohsaraho <osg21001@byui.edu>
2021-11-30 23:59:45 -0700

    starting join page

---
+++ final_project/js/menu_bar.js
@@ -0,0 +1,8 @@
+const hambutton = document.querySelector('.ham');
+const menunav = document.querySelector('.navigation');
+
+hambutton.addEventListener('click', () => {menunav.classList.toggle('responsive')}, false);
+
+
+// To solve the mid resizing issue with responsive class on
+window.onresize = () => {if (window.innerWidth > 600) menunav.classList.remove('responsive')};
