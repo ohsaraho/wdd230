b7c1bc05ee5399b89a3fca3fadf82e634418d987
ohsaraho <osg21001@byui.edu>
2021-12-03 00:35:31 -0700

    forms

---
+++ final_project/js/dates.js
@@ -0,0 +1,5 @@
+// document.getElementById("copyrightyear").textContent = new Date().getFullYear();
+
+/* document.getElementById("lastupdated").textContent = new Date(document.lastModified); */
+
+document.getElementById("last_modified").innerHTML = document.lastModified;
