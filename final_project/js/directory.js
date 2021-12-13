9c5666bf6c6e7040afacbe956e33f607f64ebc56
ohsaraho <osg21001@byui.edu>
2021-12-13 01:17:25 -0700

    started on directory page, json file plus logos

---
+++ final_project/js/directory.js
@@ -0,0 +1,49 @@
+const businessjson = 'data/businessOrganizations.json';
+
+
+
+fetch(businessjson)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (jsonObject) {
+    const businesses = jsonObject['businesses'];
+    // console.table(jsonObject);  // temporary checking for valid response and data parsing
+    businesses.forEach(business => { 
+        let card = document.createElement('section');
+        let h2 = document.createElement('h2');
+        let image = document.createElement('img');
+        let pPhone = document.createElement('p');
+        let pAddress = document.createElement('p');
+        let pCityZip = document.createElement('p');
+        let aURL = document.createElement('a');
+        let pDescript = document.createElement('p');
+        
+
+
+    h2.textContent = business.businessName;
+    image.setAttribute('src', `images/directory/${business.logo}`);
+    image.setAttribute('alt', business.businessName);
+    pPhone.textContent = business.phoneNumber;
+    pAddress.textContent = business.address;
+    pCityZip.textContent = `${business.city}, ${business.state} ${business.zipCode}`;
+    aURL.textContent = business.websiteURL;
+    aURL.setAttribute('href', `${business.websiteURL}`);
+    pDescript.textContent = business.description;
+    
+
+
+    card.appendChild(h2);
+    card.appendChild(image);
+    card.appendChild(pDescript);
+    card.appendChild(pAddress);
+    card.appendChild(pCityZip);
+    card.appendChild(pPhone);
+    card.appendChild(aURL);
+    
+
+
+    document.querySelector('div.cards').appendChild(card);
+    
+  });
+});
