3aaf481c998a7a62119570979daef2c4408fbb3e
ohsaraho <osg21001@byui.edu>
2021-12-14 10:15:00 -0700

    fixed weather alerts and resizing of text in alert

--- final_project/js/weather_api.js
+++ final_project/js/weather_api.js
@@ -1,38 +1,67 @@
-const alertMessage = document.querySelector(".weatherAlerts");
-
 const apiURL = "https://api.openweathermap.org/data/2.5/onecall?lat=40.7608&lon=-111.8910&exclude=minutely,hourly&appid=5a69da6bedb5c53762ac5a1761526a1b&units=imperial";
+
 fetch(apiURL)
   .then((response) => response.json())
   .then((jsObject) => {
     console.log(jsObject);
     
-    document.getElementById('current_weather').textContent = jsObject.current.weather[0].main;
-    document.getElementById('current_temp').textContent = `${jsObject.current.temp} °F`;
-    document.getElementById('humidity').textContent = `${jsObject.current.humidity}%`;
+    document.querySelector('#current_weather').textContent = jsObject.current.weather[0].main;
+    document.querySelector('#current_temp').textContent = `${jsObject.current.temp} °F`;
+    document.querySelector('#humidity').textContent = `${jsObject.current.humidity}%`;
 
 
-    // const alerts = jsObject['alerts'];
+    if(jsObject.alerts != null) {
+      let cityh1 = document.createElement('h1');
+      cityh1.textContent = 'Weather Alerts for the Salt Lake City Area';
+      document.querySelector('div.weatherAlerts').appendChild(cityh1);
 
-    if (jsObject.alerts != null) {
-      document.querySelector(".weatherAlerts").style.display = 'block'
-     document.querySelector('.weatherEvent').textContent = jsObject.alerts[0].event;
-     document.querySelector('.eventDescription').textContent = jsObject.alerts[0].description;
-   }else {
-    document.querySelector(".weatherAlerts").style.display = 'block'
-    document.querySelector('.weatherEvent').textContent = 'No current weather alerts' 
+      jsObject.alerts.forEach((alert) => {
+        let divWeather = document.createElement('div');
+        let event = document.createElement('h2');
+        let ulWeathther = document.createElement('ul');
+        let description = document.createElement('li');
 
-   }
+        event.textContent = alert.event;
+        description.textContent = alert.description;
 
-  // dtTimes = jsObject.daily.dt * 1000
+        divWeather.appendChild(event);
+        divWeather.appendChild(ulWeathther);
+        ulWeathther.appendChild(description);
+      
+        document.querySelector('div.weatherAlerts').appendChild(divWeather);
+        document.querySelector(".weatherAlerts").style.display = "block";
+      }
+      
 
-  
-    
-    // let day = 1
+    )}else {
+      let cityh1 = document.createElement('h1');
+      cityh1.textContent = 'Weather Alerts for the Salt Lake City Area';
+      document.querySelector('div.weatherAlerts').appendChild(cityh1);
+
+      let event = document.createElement('h2');
+      event.textContent = 'No current weather alerts';
+      document.querySelector('div.weatherAlerts').appendChild(event);
+
+      document.querySelector(".weatherAlerts").style.display = "block";
+    };
+
+
+  // This code for weather alerts only displays the first weather alert in the array and belongs to the h1, h2 and p within the weatherAlerts div. I wasn't for sure if it just needed to be one or as many as there are or if they only provide one severe weather alert, so I am keeping it in within a comment   
+  //   if (jsObject.alerts != null) {
+  //     document.querySelector(".weatherAlerts").style.display = 'block';
+  //    document.querySelector('.weatherEvent').textContent = jsObject.alerts[0].event;
+  //    document.querySelector('.eventDescription').textContent = jsObject.alerts[0].description;
+  //  }else {
+  //   document.querySelector(".weatherAlerts").style.display = 'block';
+  //   document.querySelector('.weatherEvent').textContent = 'No current weather alerts';
+
+  //  }
+
     jsObject.daily.forEach((day, index) => {
       const weekdays = ['Sun', 'Mon', 'Tues', 'Wed', 'Thurs', 'Fri', 'Sat'];
 
       const forcast_day = day.dt*1000;
-      let date = new Date(forcast_day).getDay() + 1
+      let date = new Date(forcast_day).getDay() + 1;
       const imagesrc = `https://openweathermap.org/img/wn/${day.weather[0].icon}@2x.png`;
       index += 1;
 
@@ -52,44 +81,6 @@ fetch(apiURL)
 document.querySelector('.close').addEventListener('click', () => { 
     document.querySelector(".weatherAlerts").style.display = "none"; 
 }, false);
-
-
-// if(jsObject.alerts != 'undefined' || jsObject.alerts != null) {
-      //   let cityh2 = document.createElement('h2');
-      //   h2.textContent = 'Weather Alerts for the Salt Lake City Area';
-      //   document.querySelector('div.weatherAlerts').appendChild(cityh2);
-
-      //   jsObject.alerts.forEach((alert) => {
-      //     let weathAlert = document.createElement('ul');
-      //     let event = document.createElement('li');
-      //     let description = document.createElement('li');
-
-      //     event.textContent = alert.event;
-      //     description.textContent = alert.description;
-
-      //     weathAlert.appendChild(event);
-      //     weathAlert.appendChild(description);
-
-      //     weathAlert.setAttribute('class', 'alert');
-        
-      //     document.querySelector('div.weatherAlerts').appendChild(weathAlert);
-      //     document.querySelector(".weatherAlerts").style.display = "block";
-      //   }
-        
-
-      // )}else {
-      //   let cityh2 = document.createElement('h2');
-      //   h2.textContent = 'Weather Alerts for the Salt Lake City Area';
-      //   document.querySelector('div.weatherAlerts').appendChild(cityh2);
-      //   let alert = document.createElement('ul');
-      //   let event = document.createElement('li');
-        
-      //   event.textContent = 'No current weather alerts';
-      //   alert.appendChild(event);
-      //   document.querySelector('div.weatherAlertS').appendChild(alert);
-
-      //   document.querySelector(".weatherAlerts").style.display = "block";
-      // };
 
 
   
